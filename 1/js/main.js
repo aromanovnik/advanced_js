cb0d5c37409217f4dd2a31f50a361345ffdecf3d
alekseyromanov <aromanovnik@gmail.com>
2021-08-23 22:33:15 +0300

    Lesson 1

---
+++ 1/js/main.js
@@ -0,0 +1,22 @@
+const products = [
+    {id: 1, title: 'Notebook', price: 2000},
+    {id: 2, title: 'Mouse', price: 20},
+    {id: 3, title: 'Keyboard', price: 200},
+    {id: 4, title: 'Gamepad', price: 50},
+];
+//Функция для формирования верстки каждого товара
+//Добавить в выводе изображение
+const renderProduct = ({title, price}) => { // OR item
+    return `<div class="product-item">
+                <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1603332211000" alt="${title}">
+                <h3>${title}</h3>
+                <p>${price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+};
+const renderPage = list => {
+    const productsList = list.map(item => renderProduct(item));
+    document.querySelector('.products').innerHTML = productsList.join('');
+};
+
+renderPage(products);
